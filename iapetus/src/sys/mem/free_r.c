613fbda255cba1ba0b50a4a1d1ac0dddb559734d
retrohead <retro_head@hotmail.com>
2021-02-22 02:42:36 +0000

    removed iapetus folder

--- iapetus/src/sys/mem/free_r.c
+++
@@ -1,9 +0,0 @@
-#include <sys/reent.h>
-
-#include <stdlib.h>
-
-void
-_free_r(struct _reent *r __attribute__ ((unused)), void *addr)
-{
-        free(addr);
-}
--- iapetus/src/sys/mem/malloc_r.c
+++
@@ -1,9 +0,0 @@
-#include <sys/reent.h>
-
-#include <stdlib.h>
-
-void *
-_malloc_r(struct _reent *r __attribute__ ((unused)), size_t n)
-{
-        return malloc(n);
-}
